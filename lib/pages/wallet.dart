38c36753c832d81e0e8af2fe1442e957e26d3530
Ramzy Kermad <kermad.r@sncf.fr>
2024-06-26 19:18:28 +0200

    Started implementing basing UI for the wallet screen

--- lib/pages/wallet.dart
+++ lib/pages/wallet.dart
@@ -10,13 +10,49 @@ class WalletScreen extends StatefulWidget {
 class _WalletStateScreen extends State<WalletScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      color: Colors.black,
-      child: const Center(
-        child: Text("Wallet"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Wallet",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              _scanQrcode();
+            },
+            child: const Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: Icon(
+                Icons.qr_code_scanner,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.black,
+        child: const Center(
+          child: Icon(
+            Icons.qr_code_2,
+            color: Colors.white,
+            size: 350,
+          ),
+        ),
       ),
     );
+  }
+
+  void _scanQrcode() {
+    print("Opening camera to scan qrcode");
   }
 }
